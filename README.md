f84edc52cc1ac21d3bb2062cc900e30b61a821b5
jco898 <62203980+jco898@users.noreply.github.com>
2022-11-25 18:51:05 -0500

    Update
    Added C# Tic Tac Toe game to repository.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# TicTacToe_C-Sharp
+# Tic Tac Toe (C#)
+<img src="tic-tac-toe-black-white.png" alt="tic-tac-toe" width="96px">
+
+## About
+This is a tic tac toe game made in a C# console application.
+
+Players take turns marking X's and O's on a 3x3 board, by typing their desired position.
+- A console screen indicates which player's turn it is, and updates in real-time.
+- A player wins once they match three of their X/O's in a row.
+
+## Context
+This application was made at the start Application Development I.
+
+As an introduction to MVC, we were asked to create two Tic Tac Toe games backed by a dll model we created.
+
+One of the games was made as a WPF application, while this one was made with C#.
+
+I found this as the easier game to create, and serves as a good intro to MVC.
+
+## How to run the app
+1. Open the project in Visual Studio.
+2. Click Run.
+3. The players mark their characters by entering their desires position, with error validation in place.
+    - Player 1, as X, goes first.
+    - Player 2, as O, follows.
+
+<img src="tic-tac-toe-csharp.png" alt="tic-tac-toe-csharp" width="300px;">
+
+4. This continues until either a player wins, or the match ends in a draw.
+5. After the game is over, the player must press any Key to start again.
---
+++ TicTacToeGame (C#)/TicTacToeGame/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program : ViewInterface
+    {
+        private static char[] positions = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        private static char symbol;
+        private static Presenter presenter;
+
+        static void Main(string[] args)
+        {
+            presenter = new Presenter(new Program());
+
+            while (true)
+            {
+                presenter.NewGame();
+
+                while (presenter.ValidGame())
+                {
+                    Console.Clear();
+                    Draw();
+
+                    int player = presenter.GetPlayer();
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    try
+                    {
+                        Console.WriteLine($"Player {player}, it is your turn to play");
+                        Console.WriteLine("Enter row number: ");
+                        var row = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter col number: ");
+                        var col = Int32.Parse(Console.ReadLine());
+                        
+                        presenter.ProcessChoice(player, row, col);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws the TicTacToe board with positions.
+        /// </summary>
+        private static void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", positions[0], positions[1], positions[2]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", positions[3], positions[4], positions[5]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", positions[6], positions[7], positions[8]);
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Resets all current symbols.
+        /// </summary>
+        public void Reset()
+        {
+            positions[0] = ' ';
+            positions[1] = ' ';
+            positions[2] = ' ';
+            positions[3] = ' ';
+            positions[4] = ' ';
+            positions[5] = ' ';
+            positions[6] = ' ';
+            positions[7] = ' ';
+            positions[8] = ' ';
+        }
+
+        /// <summary>
+        /// Sets the symbol (X / O) of the player in its position.
+        /// </summary>
+        /// <param name="player">The current player</param>
+        /// <param name="row">The row number</param>
+        /// <param name="col">The column number</param>
+        public void SetPosition(int player, int row, int col)
+        {
+            if (player == 1)
+            {
+                symbol = 'X';
+            }
+            else if (player == 2)
+            {
+                symbol = 'O';
+            }
+
+            switch (row, col)
+            {
+                case (0, 0):
+                    positions[0] = symbol;
+                    break;
+                case (0, 1):
+                    positions[1] = symbol;
+                    break;
+                case (0, 2):
+                    positions[2] = symbol;
+                    break;
+                case (1, 0):
+                    positions[3] = symbol;
+                    break;
+                case (1, 1):
+                    positions[4] = symbol;
+                    break;
+                case (1, 2):
+                    positions[5] = symbol;
+                    break;
+                case (2, 0):
+                    positions[6] = symbol;
+                    break;
+                case (2, 1):
+                    positions[7] = symbol;
+                    break;
+                case (2, 2):
+                    positions[8] = symbol;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Displays an error message to the console.
+        /// </summary>
+        /// <param name="error"></param>
+        public void ShowError(string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Displays a message indicating a win.
+        /// </summary>
+        /// <param name="result"></param>
+        public void ShowWin(string result)
+        {
+            Console.Clear();
+            Draw();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(result);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Displays a message indicating a draw.
+        /// </summary>
+        /// <param name="result"></param>
+        public void ShowDraw(string result)
+        {
+            Console.Clear();
+            Draw();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(result);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+    }
+}
